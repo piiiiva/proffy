21595cf642b335ca73af536663e84caadeadd9e2
piiiiva <piiiiva@gmail.com>
2020-08-05 01:25:13 -0300

    Using transaction to prevent data insertion when err; Add sqlite files to .gitignore

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -13,35 +13,47 @@ interface ScheduleItem {
 routes.post('/classes', async (request, response) => {
   const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
 
-  const insertedUsersIds = await db('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
+  const trx = await db.transaction();
 
-  const user_id = insertedUsersIds[0];
+  try {
+    const insertedUsersIds = await trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+    });
+  
+    const user_id = insertedUsersIds[0];
+  
+    const insertedClassesIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id,
+    });
+  
+    const class_id = insertedClassesIds[0];
+  
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinutes(scheduleItem.from),
+        to: convertHourToMinutes(scheduleItem.to),
+      };
+    });
+  
+    await trx('class_schedule').insert(classSchedule);
+  
+    await trx.commit();
+  
+    return response.status(201).send();
+  } catch (err) {
+    await trx.rollback();
 
-  const insertedClassesIds = await db('classes').insert({
-    subject,
-    cost,
-    user_id,
-  });
-
-  const class_id = insertedClassesIds[0];
-
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinutes(scheduleItem.from),
-      to: convertHourToMinutes(scheduleItem.to),
-    };
-  });
-
-  await db('class_schedule').insert(classSchedule);
-
-  return response.send();
+    return response.status(400).json({
+      error: 'Unexpected error while creating new class'
+    })
+  }
 });
 
 export default routes;
