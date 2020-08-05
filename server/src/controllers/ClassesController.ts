99a6ab273deeffee965631784dee2026b5152321
piiiiva <piiiiva@gmail.com>
2020-08-05 02:05:49 -0300

    Created index method to get classes

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -10,6 +10,41 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
+    if(!filters.week_day || !filters.subject || !filters.time) {
+      return response.status(400).json({
+        error: 'Missing filters to search classes'
+      })
+    };
+
+    // O usuário digita o valor em horas 09:00, temos que transformar em minutos 540 para o db entender
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db('classes')
+      .whereExists(function() { // subquery para verificar se existe um horário disponível
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`') // JOIN
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)]) // Verificar se atende no dia
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes]) // Verifica o horário que o proffy começa a trabalhar
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]) // Verifica o horário que o proffy para a trabalhar
+
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id') // INNER JOIN
+      .select(['classes.*', 'users.*']);
+
+    console.log(timeInMinutes);
+
+    return response.json(classes);
+  }
+
   async create(request: Request, response: Response) {
     const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
   
