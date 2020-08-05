a31ae535c082b1b2a3bf262cf0ef4433549076fa
piiiiva <piiiiva@gmail.com>
2020-08-04 23:06:04 -0300

    Add express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({message: 'Hello World'});
+});
+
+app.listen(3333);
+
