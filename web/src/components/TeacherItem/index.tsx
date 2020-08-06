86b02887ef1c015e0d4a7f15ba3f793612d2d7c1
piiiiva <piiiiva@gmail.com>
2020-08-06 01:47:37 -0300

    Connect with server API to post a new user connection

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -17,6 +18,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id,
+    });
+  };
+  
   return (
     <article className="teacher-item">
       <header>
@@ -34,7 +41,10 @@ const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => 
           Preço/Hora
           <strong>R$ {teacher.cost}</strong>
         </p>
-        <a href={`https://wa.me/${teacher.whatsapp}`}>
+        <a 
+          target="blank"
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
         </a>
