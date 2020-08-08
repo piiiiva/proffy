a2ee41a2e3da856d98d8a0c3b5fa5cddc8d23738
piiiiva <piiiiva@gmail.com>
2020-08-08 00:59:48 -0300

    Create form block for filter

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import PageHeader from '../../components/PageHeader';
 
 import styles from './styles'
@@ -9,7 +9,36 @@ import { ScrollView } from 'react-native-gesture-handler';
 function TeacherList() {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Qual a matéria?"
+            placeholderTextColor="#c1bccc"
+          />
+
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da Semana</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual o dia?"
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual horário?"
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+          </View>
+        </View>
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
